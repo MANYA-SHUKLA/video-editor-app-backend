58be8484a5d78ad94f67492e12a51fcbc5418d56
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-12-17 15:58:35 +0530

    your commit message

--- server.js
+++ server.js
@@ -51,8 +51,6 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
   })
 );
 app.use(express.json());
